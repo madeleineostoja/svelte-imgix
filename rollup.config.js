ad3b79e859044ad5aa8dba91218ccc9520f9b8b9
madeleineostoja <madi@madeleineostoja.com>
2021-05-21 11:25:54 +1200

    fix typescript decl output

--- rollup.config.js
+++ rollup.config.js
@@ -7,8 +7,8 @@ import pkg from './package.json';
 export default {
   input: 'src/index.ts',
   output: [
-    { file: pkg.module, format: 'es' },
-    { file: pkg.main, format: 'umd', name: 'svelte-imgix' }
+    { dir: 'dist', format: 'umd', name: 'svelte-imgix' },
+    { file: pkg.module, format: 'es' }
   ],
   plugins: [typescript(), resolve(), commonJS()]
 };
